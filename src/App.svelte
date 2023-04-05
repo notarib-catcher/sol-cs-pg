c585da9162bd94b715a94e4602806552cfac72e5
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-04-06 01:28:00 +0530

    Basic under construction page with JS canvas particles behind it

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import logo from "./assets/HALO WITHOUT THE BG SHADOW.svg"
+</script>
+<div id="container" class=" w-screen h-screen flex">
+  <div class=" m-auto w-fit block bg-opacity-40 mx-auto justify-center text-center text-white">
+    <div class=" w-fit p-10 m-auto rounded-lg bg-[#171719] mt-10 font-light bg-opacity-50 text-lg">
+    <img src={logo} alt="logo" class=" h-[300px]">
+      <div class=" mt-10">
+        <b>This website is under construction</b>
+      </div>
+    </div>
+
+  </div>
+
+</div>
